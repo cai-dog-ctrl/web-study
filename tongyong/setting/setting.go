acd3cd09268f0c3028ba02ee9ef36010e07cae8c
cai-dog-ctrl <1296643805@qq.com>
2021-11-30 17:42:01 +0800

    通用脚手架

---
+++ tongyong/setting/setting.go
@@ -0,0 +1,23 @@
+package setting
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func Init() (err error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("viper.ReadInConfig err:%v", err)
+		return
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println("配置文件修改了")
+	})
+	return
+}
